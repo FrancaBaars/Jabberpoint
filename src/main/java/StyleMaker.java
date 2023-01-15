bce21293d962a840a06a38f5f1ca8618887b0094
FrancaBaars <franca.baars@student.nhlstenden.com>
2023-01-15 19:28:26 +0100

    made method to create styles array and to make new style

--- src/main/java/StyleMaker.java
+++ src/main/java/StyleMaker.java
@@ -4,15 +4,24 @@ public class StyleMaker {
     private static Style[] styles; // de styles
 
     public static void createStyles() {
-        styles = new Style[5];
+        StyleMaker.makeStyleArray(5);
 
         // De styles zijn vast ingecodeerd.
-        styles[0] = new Style(0, Color.red, 48, 20);    // style voor item-level 0
-        styles[1] = new Style(20, Color.blue, 40, 10);    // style voor item-level 1
-        styles[2] = new Style(50, Color.black, 36, 10);    // style voor item-level 2
-        styles[3] = new Style(70, Color.black, 30, 10);    // style voor item-level 3
-        styles[4] = new Style(90, Color.black, 24, 10);    // style voor item-level 4
+        StyleMaker.styles[0] = StyleMaker.makeNewStyle(0, Color.red, 48, 20);    // style voor item-level 0
+        StyleMaker.styles[1] = StyleMaker.makeNewStyle(20, Color.blue, 40, 10);    // style voor item-level 1
+        StyleMaker.styles[2] = StyleMaker.makeNewStyle(50, Color.black, 36, 10);    // style voor item-level 2
+        StyleMaker.styles[3] = StyleMaker.makeNewStyle(70, Color.black, 30, 10);    // style voor item-level 3
+        StyleMaker.styles[4] = StyleMaker.makeNewStyle(90, Color.black, 24, 10);    // style voor item-level 4
     }
+
+    private static void makeStyleArray(int number) {
+        StyleMaker.styles = new Style[number];
+    }
+
+    private static Style makeNewStyle(int indent, Color color, int points, int leading) {
+        return new Style(indent, color, points, leading);
+    }
+
 
     public static Style getStyle(int level) {
         if (level >= StyleMaker.styles.length) {
