a65ec4e99bca9bd03fb2a126dd02d103c2da16bf
Franca <franca.baars@student.nhlstenden.com>
2023-03-28 16:16:34 +0200

    removed unused tostring method

--- src/main/java/XMLAccessor.java
+++ src/main/java/XMLAccessor.java
@@ -57,7 +57,7 @@ public class XMLAccessor implements Savable, Loadable {
 
             this.addSlidesToPresentation(doc, presentation);
         } catch (IOException iox) {
-            System.err.println(iox.toString());
+            System.err.println(iox);
         } catch (SAXException sax) {
             System.err.println(sax.getMessage());
         } catch (ParserConfigurationException pcx) {
